ca6188385986135e560bd146e433f77e247fd822
naubull2 <naubull2@gmail.com>
2023-02-23 22:46:18 +0900

    Initial commit with basic debug API

---
+++ README.md
@@ -0,0 +1,44 @@
+# rust-del-dups
+
+Rust remove-duplicate API
+
+Simply removes ngram duplicates in the input string.
+
+- Allows up to 5 unigram repetitions
+- Allows up to 3 bigram repetitions
+- Allows up to 1 N-gram repetitions (N > 2)
+
+
+## How to use
+
+Simply build using cargo, then run the API via `cargo run`
+
+```
+cargo run
+...
+Running `target/debug/src`
+🔧 Configured for debug.
+   >> address: 127.0.0.1
+   >> port: 8000
+   >> workers: 10
+   >> ident: Rocket
+   >> limits: bytes = 8KiB, data-form = 2MiB, file = 1MiB, form = 32KiB, json = 1MiB, msgpack = 1MiB, string = 8KiB
+   >> temp dir: /var/folders/6k/tjrzsxzd6_13ngrfyc6t13_w0000gp/T/
+   >> http/2: true
+   >> keep-alive: 5s
+   >> tls: disabled
+   >> shutdown: ctrlc = true, force = true, signals = [SIGTERM], grace = 2s, mercy = 3s
+   >> log level: normal
+   >> cli colors: true
+📬 Routes:
+   >> (remove_dups_handler) GET /remove-dups?<q>
+📡 Fairings:
+   >> Shield (liftoff, response, singleton)
+🛡️ Shield:
+   >> X-Frame-Options: SAMEORIGIN
+   >> Permissions-Policy: interest-cohort=()
+   >> X-Content-Type-Options: nosniff
+🚀 Rocket has launched from http://127.0.0.1:8000
+
+```
+
---
+++ src/main.rs
@@ -0,0 +1,110 @@
+#[macro_use]
+extern crate rocket;
+
+use rocket::request::FromParam;
+use rocket::serde::json::Json;
+use serde::{Serialize, Deserialize};
+
+
+#[derive(Debug)]
+struct Query(String);
+
+impl<'r> FromParam<'r> for Query {
+    type Error = &'r str;
+
+    fn from_param(param: &'r str) -> Result<Self, Self::Error> {
+        if !param.is_empty() {
+            Ok(Query(param.to_string()))
+        } else {
+            Err("Query cannot be empty.")
+        }
+    }
+}
+
+fn remove_ngram_dups(s: &str, n: usize, term: Option<usize>) -> String {
+    let max_dups = if n < 3 { 3 } else { 1 };
+    let s = s.chars().collect::<Vec<char>>();
+
+    // largest N-gram duplicate possible
+    let term = term.unwrap_or_else(|| s.len() / 2);
+
+    // terminal size
+    if (n == 0 || n > term) && !s.is_empty() {
+        return s.iter().collect();
+    }
+
+    let mut result = Vec::new();
+    for i in (0..s.len()).step_by(n) {
+        if i + (max_dups + 1) * n - 1 >= s.len() {
+            result.extend(&s[i..]);
+            break;
+        }
+        if (2..=max_dups + 1).all(|k| &s[i..i + n] == &s[i + (k - 1) * n..i + k * n]) {
+            // duplicate found
+        } else {
+            result.extend(&s[i..i + n]);
+        }
+    }
+
+    if s != result {
+        // backtrack to n/2 gram if pattern is found
+        let new_n = n / 2;
+        let new_term = Some(term);
+        remove_ngram_dups(&result.iter().collect::<String>(), new_n, new_term)
+    } else {
+        let new_n = n + 1;
+        let new_term = Some(term);
+        remove_ngram_dups(&result.iter().collect::<String>(), new_n, new_term)
+    }
+}
+
+fn remove_dups(s: &str) -> String {
+    let s1 = remove_ngram_dups(&format!(" {}", s), 1, None);
+    let s2 = remove_ngram_dups(&format!("{} ", s), 1, None);
+    if s1.len() < s2.len() {
+        return s1;
+    }
+    s2
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Output{
+    pub result: String
+}
+
+#[get("/remove-dups?<q>")]
+fn remove_dups_handler(q: &str) -> Json<Output> {
+    let result = remove_dups(q);
+
+    Json(Output{result})
+}
+
+
+#[rocket::main]
+async fn main() {
+   if let Err(err) = rocket::build()
+      .mount("/", rocket::routes![remove_dups_handler])
+      .launch()
+      .await
+   {
+      println!("Rocket Rust couldn't take off successfully!");
+      drop(err); // Drop initiates Rocket-formatted panic
+   }
+}
+
+/*
+use std::io::{self, BufRead};
+
+fn main() {
+	println!("Type any string");
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let line = line.unwrap();
+        if line.trim().is_empty() {
+            break;
+        }
+        let result = remove_dups(&line);
+        println!("{}", result);
+    }
+}
+*/
